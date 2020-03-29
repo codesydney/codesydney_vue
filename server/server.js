ce2745a943a42d42d13158a3912210e43f7b4bef
Promie Yutasane <promie.yutasane@gmail.com>
2020-03-29 22:35:14 +1100

    Removed configuration folder

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-const Mongoose = require('./config/db')
 
 const app = express()
 
@@ -11,11 +10,6 @@ app.use(express.json({ extended: true }))
 
 const PORT = process.env.PORT || 5000
 
-const startServer = async () => {
-  await Mongoose().initialiseMongoConnection()
-  app.listen(PORT, () => {
-    console.log(`App started on PORT ${PORT}`)
-  })
-}
-
-startServer()
+app.listen(PORT, () => {
+  console.log(`App started on PORT ${PORT}`)
+})
