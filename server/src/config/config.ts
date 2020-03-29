d5ad74468aa5cf3f6b8571dce12a1904fc3e6a71
Promie Yutasane <promie.yutasane@gmail.com>
2020-03-29 19:55:13 +1100

    DEV-002 changed configuration for the tsconfig.json file

--- server/src/config/config.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-
-dotenv.config({ path: './config/config.env' })
-
-export default {
-  port: process.env.PORT,
-  nodeEnvironment: process.env.NODE_ENV,
-  mongoURI: process.env.MONGOURI
-}
