df7ace3ba6493cd43ed59b9aeaaa4da6ebf6cffc
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-04 16:05:24 +1100

    DEV-004 creat the POST request for mentor

--- server/controllers/MentorController.js
+++ server/controllers/MentorController.js
@@ -1,13 +1,29 @@
+const Mentor = require('../models/Mentor')
+
 const MentorController = () => {
-  const createMentor = (req, res, next) => {
-    console.log('MENTOR POST')
+  const createMentor = async (req, res, next) => {
+    try {
+      const mentor = await Mentor.create(req.body)
+
+      res.status(201).json({
+        status: 'success',
+        data: {
+          mentor,
+        },
+      })
+    } catch (err) {
+      res.status(500).json({
+        status: 'failed',
+        message: err.message,
+      })
+    }
   }
 
   const getMentors = (req, res, next) => {
     console.log('GET ALL MENTORS')
   }
 
-  return { createMentor, getMentors}
+  return { createMentor, getMentors }
 }
 
 module.exports = MentorController
