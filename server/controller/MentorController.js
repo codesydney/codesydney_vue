bb9bd7a82490d4449f1a09bc82b347b75ed2c407
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-05 12:46:58 +1000

    DEV-005 create catchAsync function

--- server/controller/MentorController.js
+++ server/controller/MentorController.js
@@ -1,23 +1,17 @@
+const catchAsync = require('../utils/catchAsync')
 const Mentor = require('../model/Mentor')
 
 const MentorController = () => {
-  const createMentor = async (req, res, next) => {
-    try {
-      const mentor = await Mentor.create(req.body)
+  const createMentor = catchAsync(async (req, res, next) => {
+    const mentor = await Mentor.create(req.body)
 
-      res.status(201).json({
-        status: 'SUCCESS',
-        data: {
-          mentor,
-        },
-      })
-    } catch (error) {
-      res.status(500).json({
-        status: 'FAIL',
-        message: error.message,
-      })
-    }
-  }
+    res.status(201).json({
+      status: 'SUCCESS',
+      data: {
+        mentor,
+      },
+    })
+  })
 
   const getMentors = async (req, res, next) => {
     try {
