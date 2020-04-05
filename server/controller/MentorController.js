edfb2a5b1e79de5d328f9eab95d906a5bda2b44d
Julia Koldorkina <yuliya.cold@gmail.com>
2020-04-05 15:36:07 +1000

    DEV-005: add GET UPDATE and DELETE mentor

--- server/controller/MentorController.js
+++ server/controller/MentorController.js
@@ -3,48 +3,72 @@ const Mentor = require('../model/Mentor')
 
 const MentorController = () => {
   const createMentor = catchAsync(async (req, res, next) => {
-    const mentor = await Mentor.create(req.body)
+    const mentor = await Mentor.create(req.body);
 
     res.status(201).json({
       status: 'SUCCESS',
       data: {
         mentor,
       },
-    })
-  })
+    });
+  });
 
-  const getMentors = async (req, res, next) => {
-    try {
-      const mentors = await Mentor.find()
+  const getMentors = catchAsync(async (req, res, next) => {
+    const mentors = await Mentor.find();
+
+    res.status(200).json({
+      status: 'SUCCESS',
+      data: {
+        mentors,
+      },
+    });
+  });
+
+  // use the catchAsyc on all function in controller
+  const getMentor = catchAsync(async (req, res, next) => {
+    const mentor = await Mentor.findById(req.params.mentorId);
+
+    res.status(200).json({
+      status: 'SUCCESS',
+      data: {
+        mentor,
+      },
+    });
+  });
+
+  const updateMentor = catchAsync(async (req, res, next) => {
+    const mentor = await Mentor.findByIdAndUpdate(
+      req.params.mentorId,
+      req.body,
+      {new: true},
+    );
+
+    res.status(200).json({
+      status: 'SUCCESS',
+      data: {
+        mentor,
+      },
+    });
+  });
+
+  const deleteMentor = catchAsync(async (req, res, next) => {
+    const mentor = await Mentor.findByIdAndRemove(req.params.mentorId);
+
+    if (mentor) {
       res.status(200).json({
         status: 'SUCCESS',
         data: {
-          mentors,
+          mentor,
         },
-      })
-    } catch (error) {
-      res.status(500).json({
-        status: 'FAIL',
-        message: error.message,
-      })
+      });
+    } else {
+      res.status(404).json({
+        status: 'NOT FOUND',
+      });
     }
-  }
+  });
 
-  // use the catchAsyc on all function in controller
-
-  const getMentor = (req, res, next) => {
-    console.log('Get Montor')
-  }
-
-  const updateMentor = (req, res, next) => {
-    console.log('Get Montor')
-  }
-
-  const deleteMentor = (req, res, next) => {
-    console.log('Get Montor')
-  }
-
-  return { createMentor, getMentors }
-}
+  return {createMentor, getMentors, getMentor, deleteMentor, updateMentor};
+};
 
 module.exports = MentorController
--- server/route/mentor.js
+++ server/route/mentor.js
@@ -2,12 +2,18 @@ const express = require('express')
 const MentorController = require('../controller/MentorController')
 
 const router = express.Router()
+const mentorController = MentorController()
 
 router
   .route('/')
-  .post(MentorController().createMentor)
-  .get(MentorController().getMentors)
+  .post(mentorController.createMentor)
+  .get(mentorController.getMentors)
 
 // Create a route to get a single mentor
+router
+  .route('/:mentorId')
+  .get(mentorController.getMentor)
+  .delete(mentorController.deleteMentor)
+  .put(mentorController.updateMentor)
 
 module.exports = router
