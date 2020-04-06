20f56e191da4cc77cdf5607775a805f108f9f916
Promie Yutasane <promie.yutasane@gmail.com>
2020-04-06 19:13:29 +1000

    DEV-012 Add timestamp to the user model

--- server/model/Mentor.js
+++ server/model/Mentor.js
@@ -1,20 +1,25 @@
 const mongoose = require('mongoose')
 
-const MentorSchema = new mongoose.Schema({
-  photo: Boolean,
-  fullName: {
-    type: String,
-    required: true,
+const MentorSchema = new mongoose.Schema(
+  {
+    photo: Boolean,
+    fullName: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
   },
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-})
+  {
+    timestamps: true,
+  }
+)
 
 MentorSchema.set('toJSON', {
   transform: (document, returnedObject) => {
