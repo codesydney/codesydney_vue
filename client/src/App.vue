13c2b2942c891a5ec242fd027d235c859df46582
Max Du <social.nsw@gmail.com>
2020-07-23 19:13:27 +1000

    [Mod] style the li

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <!-- Banner/Header -->
-    <!-- Hamburger -->
-    <!-- <div id="myNav" :class="{ navbar_open: showNavbar }" class="navbar"> -->
     <div id="myNav" :class="{ navbar_open: showNavbar, navbar: !showNavbar }">
       <div class="closebtn" @click="showNavbar = !showNavbar">
         <i class="fa fa-times"></i>
       </div>
       <div class="navbar-content">
-        <a>Home</a>
-        <a>Contact</a>
+        <ul>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
       </div>
     </div>
     <div class="openbtn">
@@ -25,13 +29,24 @@ export default {
     return {
       showNavbar: false
     };
-  },
-  methods: {}
+  }
 };
 </script>
 
 <style>
 @import url('https://use.fontawesome.com/releases/v5.9.0/css/all.css');
+
+.navbar {
+  height: 0%;
+  width: 100%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  overflow-y: hidden;
+  transition: 0.3s;
+  background-color: #2b4c72;
+}
 
 .navbar_open {
   width: 100%;
@@ -40,22 +55,6 @@ export default {
   z-index: 1;
   top: 0;
   left: 0;
-  background-color: rgb(0, 0, 0);
-  background-color: rgba(0, 0, 0, 0.9);
-  overflow-y: hidden;
-  transition: 0.5s;
-  background-color: #2b4c72;
-}
-
-.navbar {
-  width: 100%;
-  height: 0%;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: rgb(0, 0, 0);
-  background-color: rgba(0, 0, 0, 0.9);
   overflow-y: hidden;
   transition: 0.5s;
   background-color: #2b4c72;
@@ -65,15 +64,26 @@ export default {
   position: relative;
   top: 25%;
   width: 100%;
+  font-size: 2rem;
   text-align: center;
   margin-top: 30px;
+}
+
+.navbar_open ul li {
+  list-style: none;
+  color: white;
+  text-align: center;
+}
+
+.navbar_open ul li:hover {
+  cursor: pointer;
+  color: yellow;
 }
 
 .navbar a {
   padding: 8px;
   text-decoration: none;
-  font-size: 36px;
-  color: #818181;
+  color: white;
   display: block;
   transition: 0.3s;
 }
@@ -83,17 +93,19 @@ export default {
   color: #f1f1f1;
 }
 
-.closebtn {
+.navbar_open .closebtn {
   position: absolute;
   top: 20px;
   left: 45px;
-  font-size: 60px;
+  font-size: 2rem;
   color: white;
   cursor: pointer;
 }
 
 .openbtn i {
-  font-size: 30px;
+  top: 20px;
+  left: 45px;
+  font-size: 2rem;
   cursor: pointer;
 }
 
