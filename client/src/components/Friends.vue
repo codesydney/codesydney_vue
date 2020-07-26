1d8407c9070a658d306c6840809f93be1d3fe671
Promie Yutasane <promie.yutasane@gmail.com>
2020-07-26 22:07:45 +1000

    I31 constructed the friends object in constant file

---
+++ client/src/components/Friends.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <pre>
+      {{ friends }}
+    </pre>
+  </div>
+</template>
+
+<script>
+import { friends } from '@/constants.js';
+
+export default {
+  name: 'Friends',
+  computed: {
+    friends() {
+      return friends;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -4,13 +4,17 @@
     <h1>Hello World!</h1>
     <!-- Member Previews -->
     <MemberPreview />
+    <Friends />
   </div>
 </template>
 
 <script>
+import Friends from '@/components/Friends.vue';
 import MemberPreview from '@/components/MemberPreview.vue';
+
 export default {
   components: {
+    Friends,
     MemberPreview
   }
 };
