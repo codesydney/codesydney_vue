ce31d434b51db1b588b9280336945f8235edb4f3
Promie Yutasane <promie.yutasane@gmail.com>
2020-07-29 20:54:47 +1000

    I31 controlling media query for padding and font size

--- client/src/components/Friends.vue
+++ client/src/components/Friends.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <pre>
-      {{ friends }}
-    </pre>
+    <div class="friends-container">
+      <div>Friends</div>
+    </div>
   </div>
 </template>
 
@@ -20,11 +20,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.friends-container {
+  color: #fff;
+  background-color: #224365;
+  text-align: center;
+  margin: auto;
+}
+
 @media screen and (max-width: 600px) {
   /* Mobile View */
+  .friends-container {
+    padding: 10px;
+    div {
+      font-size: 25px;
+    }
+  }
 }
 
 @media screen and (min-width: 900px) {
   /* Desktop View */
+  .friends-container {
+    padding: 20px;
+    div {
+      font-size: 40px;
+    }
+  }
 }
 </style>
