038070f1ca49873860e6df8892368b490cd129de
chintan_1881 <chintan123@gmail.com>
2020-07-31 11:33:51 +1000

    CalendyInt

---
+++ client/src/components/CalendyWidget.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div class="col-lg-12">
+      <h3>Book a 30-minute meeting</h3>
+      <!-- Calendly inline widget begin -->
+      <div
+        class="calendly-inline-widget"
+        data-url="https://calendly.com/codesydney"
+        style="min-width:320px;height:630px;"
+      ></div>
+      <!-- Calendly inline widget end -->
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,25 +1,25 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Contact from "../views/Contact.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import Contact from '../views/Contact.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/contact",
-    name: "Contact",
+    path: '/contact',
+    name: 'Contact',
     component: Contact
   }
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   routes
 });
 
