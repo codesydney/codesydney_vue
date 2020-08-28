56c6435a447aa4c9a03c24eb570f74af23eaae91
Qj Stout <QjStout@users.noreply.github.com>
2020-08-28 12:10:18 +1000

    Amendment to #70 move-friends
    An error slipped through all the merges from yesterday's PRs.
    Just had to remove the references to the Friends component in the Home view now that Friends is a view.

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -13,14 +13,12 @@
 import Banner from '@/components/Banner.vue';
 import Headline from '@/components/Headline.vue';
 import MemberPreview from '@/components/MemberPreview.vue';
-import Friends from '@/components/Friends.vue';
 
 export default {
   components: {
     Banner,
     Headline,
     MemberPreview,
-    Friends,
   },
 };
 </script>
