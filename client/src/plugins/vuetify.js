6bf8d0079ea2eb27cd7fd0bfd34ed0cab4af361c
kimchirice <kimchi@u12.co>
2020-09-21 12:09:16 +1000

    [Add] customized color theme, inculding primary/secondary /accent, and light and dark respectively

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -7,5 +7,16 @@ export default new Vuetify({
   icons: {
     iconfont: 'fa',
   },
-  
+
+  theme: {
+    primary: '#191970',
+    primaryLight: '#4f419F',
+    primaryDark: '#000044',
+    secondary: '#3181B9',
+    secondaryLight: '#6BB0EC',
+    secondaryDark: '#005589',
+    accent: '#2DB6E3',
+    accentLight: '#72e8ff',
+    accentDark: '#0086B1',
+  },
 });
